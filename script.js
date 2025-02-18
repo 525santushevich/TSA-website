98a12c95da4ce81cffa1651271d65d8798425662
525santushevich <525santushevich@frhsd.com>
2025-02-18 13:47:16 -0500

    made about page and tried out new marquee
    Co-authored-by: Nessa <Nessa0716@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,58 +1,10 @@
-function generateTeamCards() {
-  console.log("Generating team cards...");
-  const teamCardsContainer = document.getElementById("teamCards");
-  if (!teamCardsContainer) {
-    console.error("Team cards container not found!");
-    return;
-  }
-
-  teamMembers.forEach((member) => {
-    const card = document.createElement("div");
-    card.classList.add("col-md-4");
-
-    let backgroundColor = "#cccccc";
-
-    card.innerHTML = `
-        <div class="card">
-            <div class="card-header">
-                ${member.name}
-            </div>
-            <img src="${member.imageURL}" class="image-fluid">
-            <div class="card-body" style="background-color:${backgroundColor};">
-                <p><strong>Information:</strong> ${member.Information}</p>
-            </div>
-        </div>
-      `;
-
-    // If the member has items, create a bullet list
-    if (member.items) {
-      const bulletList = createBulletList(member.items);
-      card.querySelector(".card-body").appendChild(bulletList);
-
-      // Adding links for Financial Literacy Resources
-      if (member.links) {
-        const budgetingLink = document.createElement("p");
-        budgetingLink.innerHTML = `<a href="${member.links.budgeting}" target="_blank">Budgeting Tools</a>`;
-
-        const costLink = document.createElement("p");
-        costLink.innerHTML = `<a href="${member.links.costOfAttendance}" target="_blank">Cost of Attendance</a>`;
-
-        const workshopsLink = document.createElement("p");
-        workshopsLink.innerHTML = `<a href="${member.links.workshops}" target="_blank">Financial Literacy Workshops</a>`;
-
-        card.querySelector(".card-body").appendChild(budgetingLink);
-        card.querySelector(".card-body").appendChild(costLink);
-        card.querySelector(".card-body").appendChild(workshopsLink);
-      }
-    }
-
-    teamCardsContainer.appendChild(card);
-  });
-}
-
 const hamburger = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("hide");
 });
+
+const marquee = document.getElementById("marquee");
+const clone = marquee.cloneNode(true);
+marquee.parentElement.appendChild(clone);
